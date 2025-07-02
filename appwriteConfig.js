ad9563d3f208f45f619ea89a9be0cd99b2a3e7b0
Mashrfee Aryan <aryanmashrafee07@gmail.com>
2025-07-02 17:55:36 -0500

    Changes

---
+++ appwriteConfig.js
@@ -0,0 +1,16 @@
+import { Client, Account, Databases } from "react-native-appwrite";
+
+
+
+export const client = new Client()
+    .setEndpoint('https://nyc.cloud.appwrite.io/v1')
+    .setProject('685e1abf001d44e68831')
+
+
+
+
+export const account = new Account(client)
+export const databases = new Databases(client)
+
+export const DatabaseID = "6865b340003474dd7214"
+export const userCollectionID = "6865b340003474dd7214"
